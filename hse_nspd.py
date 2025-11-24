6455738a42b4eeda366c9526f2ea21fbf737b044
stepanosokin <stepanosokin@gmail.com>
2025-11-24 11:05:43 +0300

    добавил функцию download_nspd_settlements для скачивания нас.пунктов без аутентификации

--- hse_nspd.py
+++ hse_nspd.py
@@ -64,6 +64,76 @@ def request_refresh_token(s: Session, refresh_token=''):
         raise Exception(f"Не удалось обновить токен. Результат запроса: {result.content}")    
 
 
+def download_nspd_settlements(s: Session, tiles_gpkg='tiles.gpkg', tiles_layer='kaluga', width=512, height=512, i_from=0, i_to=512, j_from=0, j_to=512, pixel_step=3):    
+    current_dir = os.getcwd()
+    tiles_gpkg_fullpath = os.path.join(current_dir, tiles_gpkg)
+    if os.path.exists(tiles_gpkg_fullpath):
+        geojson_result = {
+            "type": "FeatureCollection",
+            "features": []
+        }
+
+        # driver = ogr.GetDriverByName('GPKG')
+        gdf = pd.read_file(tiles_gpkg_fullpath, layer=tiles_layer)
+        # ds = driver.Open(tiles_gpkg_fullpath)
+        # layer = ds.GetLayer(tiles_layer)
+        if layer:
+            url = "https://nspd.gov.ru/api/aeggis/v3/36281/wms"
+            params = {
+                "REQUEST": "GetFeatureInfo",
+                "SERVICE":"WMS",
+                "VERSION": "1.3.0",
+                "FORMAT": 'image/png',
+                "STYLES": "",
+                "TRANSPARENT": "true",
+                "LAYERS": "36281",
+                "RANDOM": "0.4158997836664142",
+                "INFO_FORMAT": "application/json",
+                "FEATURE_COUNT": "10",
+                "I": "0",
+                "J": "0",
+                "WIDTH": str(width),
+                "HEIGHT": str(height),
+                "CRS": "EPSG:3857",
+                "BBOX": "7592337.145509988,8000941.147561606,7670608.662474008,8079212.664525626",
+                "QUERY_LAYERS": "36281"
+                }
+            pass
+            # for grid_feature in layer:
+            for index, row in tqdm(gdf.iterrows(), desc='tiles loop', total=gdf.shape[0]):
+                xmin, ymin, xmax, ymax = row['geometry'].bounds
+                # extent = grid_feature.geometry().GetEnvelope()
+                # xmin, xmax, ymin, ymax = extent
+                headers = {
+                    "Referer": f"https://nspd.gov.ru/map?thematic=Default&zoom=14.087600258143208&coordinate_x={str((xmin + xmax) / 2)}&coordinate_y={str((ymin + ymax) / 2)}&theme_id=1&is_copy_url=true&active_layers=36281"
+                    }
+                params["BBOX"] = f"{xmin},{ymin},{xmax},{ymax}"
+                for i in range(i_from, i_to + 1, pixel_step):
+                    params["I"] = str(i)
+                    for j in range(j_from, j_to + 1, pixel_step):
+                        params["J"] = str(j)                        
+                        status, result = smart_http_request(s, url=url, params=params, headers=headers)
+                        if status == 200:
+                            jdata = result.json()
+                            # geojson_result["features"].extend(jdata["features"])
+                            for feature in jdata["features"]:
+                                # if feature["properties"]["options"]["guid"] not in [x["properties"]["options"]["guid"] for x in geojson_result.get("features")]:
+                                #     geojson_result["features"].append(feature)
+                                if feature["properties"]["options"]["guid"] not in [x["properties"]["guid"] for x in geojson_result.get("features")]:
+                                    for k, v in feature["properties"]["options"].items():
+                                        feature["properties"][k] = v
+                                    feature["properties"].pop('options', None)
+                                    geojson_result["features"].append(feature)
+                pass
+            geojson_result_path = os.path.join(current_dir, 'results', f"{tiles_layer}.json")
+            with open(geojson_result_path, 'w', encoding='utf-8') as of:
+                json.dump(geojson_result, of, ensure_ascii=False)
+                return True
+        pass
+    
+    return False
+
+
 def download_nspd_layer(
     nspd_layer='36281', layer_alias='result',
     tiles_gpkg='tiles.gpkg', tiles_layer='khmao',
@@ -233,16 +303,17 @@ def download_nspd_layer(
 if __name__ == '__main__':
     # Это пример отдельных слоев, которые можно парсить. Функции download_nspd_layer надо кроме прочего подать на вход id слоя
     nspd_layers = [
-        {"shortname": "югра_маг_труб", "id": "847064", "fullname": "ЮГРА - Магистральные трубопроводы для транспортировки жидких и газообразных углеводородов"},
-        {"shortname": "югра_доб_транс_газ", "id": "848517", "fullname": "ЮГРА - Объекты добычи и транспортировки газа"},
-        {"shortname": "югра_распред_труб_газ", "id": "844383", "fullname": "ЮГРА - Распределительные трубопроводы для транспортировки газа"},
-        {"shortname": "югра_доб_транс_ж_ув", "id": "847799", "fullname": "ЮГРА - Объекты добычи и транспортировки жидких углеводородов"},
-        {"shortname": "югра_труб_ж_ув", "id": "847076", "fullname": "ЮГРА - Трубопроводы жидких углеводородов"},
-        {"shortname": "югра_уч_доп_пи_и_др", "id": "847178", "fullname": "ЮГРА - Участки недр, предоставленных для добычи полезных ископаемых, а также в целях, не связанных с их добычей"},
-        {"shortname": "югра_местор_пи_пол", "id": "847284", "fullname": "ЮГРА - Месторождения и проявления полезных ископаемых (полигон)"},
-        {"shortname": "югра_местор_пи_тчк", "id": "848623", "fullname": "ЮГРА - Месторождения и проявления полезных ископаемых (точка)"},
-        {"shortname": "югра_функц_зон", "id": "847282", "fullname": "ЮГРА - Функциональные зоны"},
-        {"shortname": "сан_курорт", "id": "848566", "fullname": "Объекты санаторно-курортного назначения"}
+        # {"shortname": "югра_маг_труб", "id": "847064", "fullname": "ЮГРА - Магистральные трубопроводы для транспортировки жидких и газообразных углеводородов"},
+        # {"shortname": "югра_доб_транс_газ", "id": "848517", "fullname": "ЮГРА - Объекты добычи и транспортировки газа"},
+        # {"shortname": "югра_распред_труб_газ", "id": "844383", "fullname": "ЮГРА - Распределительные трубопроводы для транспортировки газа"},
+        # {"shortname": "югра_доб_транс_ж_ув", "id": "847799", "fullname": "ЮГРА - Объекты добычи и транспортировки жидких углеводородов"},
+        # {"shortname": "югра_труб_ж_ув", "id": "847076", "fullname": "ЮГРА - Трубопроводы жидких углеводородов"},
+        # {"shortname": "югра_уч_доп_пи_и_др", "id": "847178", "fullname": "ЮГРА - Участки недр, предоставленных для добычи полезных ископаемых, а также в целях, не связанных с их добычей"},
+        # {"shortname": "югра_местор_пи_пол", "id": "847284", "fullname": "ЮГРА - Месторождения и проявления полезных ископаемых (полигон)"},
+        # {"shortname": "югра_местор_пи_тчк", "id": "848623", "fullname": "ЮГРА - Месторождения и проявления полезных ископаемых (точка)"},
+        # {"shortname": "югра_функц_зон", "id": "847282", "fullname": "ЮГРА - Функциональные зоны"},
+        # {"shortname": "сан_курорт", "id": "848566", "fullname": "Объекты санаторно-курортного назначения"}
+        {"shortname": "зоуит", "id": "37581", "fullname": "Объекты санаторно-курортного назначения"}
     ]
     
     # Перед использованием нужно зайти на https://nspd.gov.ru/map браузером, залогиниться, включить нужный слой на карте,
@@ -259,27 +330,27 @@ if __name__ == '__main__':
        
     # ЗАМЕНИТЬ!!!
     # cookie = '_ym_uid=1757065220249938669; _ym_d=1757065220; _ym_isad=1; _ym_visorc=w; authAccessToken=eyJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiIwMzM4NmRiZS01MTg1LTRiOTYtOTAwZC1jMmIxYmIzNjhlZDYiLCJjbGllbnRfaWQiOiIwMzM4NmRiZS01MTg1LTRiOTYtOTAwZC1jMmIxYmIzNjhlZDYiLCJkaXNwbGF5X25hbWUiOiLQntGB0L7QutC40L0g0KHRgtC10L_QsNC9INCQ0YDRgtC10LzQvtCy0LjRhyIsImVtYWlsIjoic3RlcGFub3Nva2luQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZXhwIjoxNzU3MDcyMzc3LCJleHBpcmVzX2luIjoxODAwLCJmYW1pbHlfbmFtZSI6ItCe0YHQvtC60LjQvSIsImdpdmVuX25hbWUiOiLQodGC0LXQv9Cw0L0iLCJpYXQiOjE3NTcwNzA1NzcsImlzcyI6Imh0dHBzOi8vc3NvLm5zcGQuZ292LnJ1IiwianRpIjoiNTViZWM3MWQtMGQ0MC00NWNkLWE5Y2ItODEyODQyMjk0Mzc1IiwibG9jYWxlIjoiZW4iLCJtaWRkbGVfbmFtZSI6ItCQ0YDRgtC10LzQvtCy0LjRhyIsIm5hbWUiOiLQntGB0L7QutC40L0g0KHRgtC10L_QsNC9INCQ0YDRgtC10LzQvtCy0LjRhyIsInByZWZlcnJlZF91c2VybmFtZSI6InAtMTIxLTI5OS02MTcgNTEiLCJzY29wZSI6InByb2ZpbGUgZW1haWwgdWlkIiwic2lkIjoiNWJhYTI2ZDktNTMyMy00MGU1LTgxOTktMzZjOGY3MWVhNTRjIiwic3ViIjoiMjQxNWUzM2ItZmU4OC00Y2IzLWE4ZTctNDIyNjQyOTU0OGE5IiwidWlkIjoiMjQxNWUzM2ItZmU4OC00Y2IzLWE4ZTctNDIyNjQyOTU0OGE5IiwidXBkYXRlZF9hdCI6MTc1NzA3MDU3NX0.FPMw5VrSODeJ7d4EiOYMHFlNaS9FMCg7LCveRrloD0qhJNZVT-jC47G2kprQQw_HUbLbQSHPNER95FOdjbk4RfusqBfAHr7_cx4L7J9EPJL8hMOk5qKqxNn_uX84NmgBD7Nz8pyuY_JMWIWhRurle6ntYkOUy0x9L9mHfVFmEQXKt_MTTmVdgmIfI6imKluB8jiWbbBVOEm2oktdscU5epgLQeMPRLBC9rBcq3mgrLuYSLXkcxyEdIZ_V16xpzlcyLUNDCo_nR4Y-PcmlOzRQW84LuQs-xNpoEWlalhr6Zs2NQ-2Y-PSHfppe2VPzbi4-rXrcGsAgBTtrI5iy--YLLlkg8H_qZNxYhSyRXTIP8-cSc1fFU0jfD6pD3oF1KJvERqiUoqYfXLRRNkD3Mk8DLVvWtdTU5Sa3_qPNMfefcrSjht4EZayK_I5azSgedjFT3JVTgOBkHx3SX3GE2i-SdKS5OviTHjCIka-JjE1a4utWAqkJAAyPeD-YJ5tKGDjpiwystuyktzM1vpvscG5Yw590BrKxmXWjjQY79ykjVugg3dzWCNa9RDlEhgnBvxpKVn9M-PQ7M6PPpF-qdKhEWw9BlhfT3wvbAjllGJosszjScfQ7VQ2iDXe7v5ZvEMVrdDB9hjOXkkDKjuTmwfu-lYDeWjOF8hJTZodae6eXRI; authAccessTokenExpires=%222025-09-05T11%3A39%3A37.493Z%22; authRefreshToken=c7MJZk9xIUqKX5HwR6mLWpBS4HEvjUbeAStYhBnZiMvB7oMzQVqevP5IGbuEbJ6VlNkKA6WZLpmlCB6not'
-    access_token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiIwMzM4NmRiZS01MTg1LTRiOTYtOTAwZC1jMmIxYmIzNjhlZDYiLCJjbGllbnRfaWQiOiIwMzM4NmRiZS01MTg1LTRiOTYtOTAwZC1jMmIxYmIzNjhlZDYiLCJkaXNwbGF5X25hbWUiOiLQntGB0L7QutC40L0g0KHRgtC10L_QsNC9INCQ0YDRgtC10LzQvtCy0LjRhyIsImVtYWlsIjoic3RlcGFub3Nva2luQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZXhwIjoxNzU3MDc0MTc3LCJleHBpcmVzX2luIjoxODAwLCJmYW1pbHlfbmFtZSI6ItCe0YHQvtC60LjQvSIsImdpdmVuX25hbWUiOiLQodGC0LXQv9Cw0L0iLCJpYXQiOjE3NTcwNzIzNzcsImlzcyI6Imh0dHBzOi8vc3NvLm5zcGQuZ292LnJ1IiwianRpIjoiOTU4ZjU4MDktYzBkZS00NmVkLWI4NjQtMmMzYjQwN2I4ODg0IiwibG9jYWxlIjoiZW4iLCJtaWRkbGVfbmFtZSI6ItCQ0YDRgtC10LzQvtCy0LjRhyIsIm5hbWUiOiLQntGB0L7QutC40L0g0KHRgtC10L_QsNC9INCQ0YDRgtC10LzQvtCy0LjRhyIsInByZWZlcnJlZF91c2VybmFtZSI6InAtMTIxLTI5OS02MTcgNTEiLCJzY29wZSI6InByb2ZpbGUgZW1haWwgdWlkIiwic2lkIjoiNWJhYTI2ZDktNTMyMy00MGU1LTgxOTktMzZjOGY3MWVhNTRjIiwic3ViIjoiMjQxNWUzM2ItZmU4OC00Y2IzLWE4ZTctNDIyNjQyOTU0OGE5IiwidWlkIjoiMjQxNWUzM2ItZmU4OC00Y2IzLWE4ZTctNDIyNjQyOTU0OGE5IiwidXBkYXRlZF9hdCI6MTc1NzA3MDU3NX0.AzNAuxT4VaQ08OqVr_deVoppF0EmtZCCPkaIazBAbS4Q9VHSXAMU0GzkkTjyFXXcC6xZGtzCreXlvtzAeKBX1G2gyrBqhpObKjvhfjtUIM8CjltgYwjTcf3q5JWkNveTP8EjMrYWxNmnJJa04gG8cXLpnClqd9ns4oXcWlk7UcSkO4xBlpCHGdMxTU_XjI7mAXjIiVh6uXXfjZVC7bvVCLMWyVq01nM1M3aUila6cMb-lOJP0mf34KUYoTGiN4bwXRrW5b7m4wLlZ3ShBvtTvU20G_n05ao_ffS05DHoz3Nbh50QtnDI7LlFbJ5RCikRLYP9KBk6CstsORQ6ku5iRhiPBNho2b7DooTaDrY9KuX4TO8dCFIsWoRzZRrro4KO1y0NHg3QZqwws2HU5CYTs9K_dB_f-rihhvcsYcNa6ygMq3QpoBdPMx6BqFGzPc-rAXVEmcD4ahvt_NT_9Y9KV0jUo10kRXIQjjAiv4wLmwO5YsrulMhHRZ9a5-UIAq4Slrv3DnlsgJOXvKKHEqBNsgoQFMdUisqpVZ9SApckdhhj_8mZYGYsmRuej4X3ICtZrBdZvYyOpjSa173344fcmpy-3IRVvFA10c48vQeQQ9wZZqCGyimd1myWY39GTUd4dmeAVMUG6Wrm1gjpRJGL80IAheklOSBbenpmG0ohUnQ'
-    auth_access_token_expires = '%222025-09-05T12%3A09%3A37.908Z%22'
-    refresh_token = 'c7MJb0g1hFrZdKxuRCR6GrMIJP8zxjbVkCH3PIooOlirrL0VWBx9vOkx6L7p14wRDprC4unkeYSWYGfLh4'    
+    access_token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiIwMzM4NmRiZS01MTg1LTRiOTYtOTAwZC1jMmIxYmIzNjhlZDYiLCJjbGllbnRfaWQiOiIwMzM4NmRiZS01MTg1LTRiOTYtOTAwZC1jMmIxYmIzNjhlZDYiLCJkaXNwbGF5X25hbWUiOiLQntGB0L7QutC40L0g0KHRgtC10L_QsNC9INCQ0YDRgtC10LzQvtCy0LjRhyIsImVtYWlsIjoic3RlcGFub3Nva2luQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZXhwIjoxNzYzNjQ0ODczLCJleHBpcmVzX2luIjoxODAwLCJmYW1pbHlfbmFtZSI6ItCe0YHQvtC60LjQvSIsImdpdmVuX25hbWUiOiLQodGC0LXQv9Cw0L0iLCJpYXQiOjE3NjM2NDMwNzMsImlzcyI6Imh0dHBzOi8vc3NvLm5zcGQuZ292LnJ1IiwianRpIjoiNDk1MzkxMWYtYmQ0Yi00MzljLTlhYjMtNGFmYjRiMDk4MmIyIiwibG9jYWxlIjoiZW4iLCJtaWRkbGVfbmFtZSI6ItCQ0YDRgtC10LzQvtCy0LjRhyIsIm5hbWUiOiLQntGB0L7QutC40L0g0KHRgtC10L_QsNC9INCQ0YDRgtC10LzQvtCy0LjRhyIsInByZWZlcnJlZF91c2VybmFtZSI6InAtMTIxLTI5OS02MTcgNTEiLCJzY29wZSI6InByb2ZpbGUgZW1haWwgdWlkIiwic2lkIjoiZTVhNGFlNjktYjcyNS00YmM4LTllMTMtYzc4MWE4MGJjMDQ2Iiwic3ViIjoiMjQxNWUzM2ItZmU4OC00Y2IzLWE4ZTctNDIyNjQyOTU0OGE5IiwidWlkIjoiMjQxNWUzM2ItZmU4OC00Y2IzLWE4ZTctNDIyNjQyOTU0OGE5IiwidXBkYXRlZF9hdCI6MTc2MzY0MzA3Mn0.w2F0Bciccr4V-4nCBUAbzsrXoEe3KjTeX243dK8hvcOnkVRigYWBBoU7WVP9NIu1jpdAjg9A7_GBYycpF11irrv3k7eX3gEOeP6GBIu9C-aOOUvEpryZyXT89d0CquzzOjKc8Leaj-NS5jlwEyUeHVpU834BrWBw4fjS1tiAjiozmXlpTceDwV02rpQrp2UcPmVoboP1MMPPN8rk20MSNyVa21wd_oIe-eRcIyZHLFaJBcOMRE-OVW_3H-yfjBDkHUipTcLJP4EjZQrmFXp0znQunxqWezh6ecWVYSNEEEXXrK9fhAba9dLPSqqoDTHS0Pw6kxTd7_kkAYJS2VToznu6uFtH_V_2EcXCu3inMw8nFEjf8KqVoP-48X_F_ThLZRAtiggTxz9KF8RVyS9rja6hYm4hityk5GMG6L4FHHUbKZoauv40_x0t5P35lV-gktHECx0ZRr2x16ElCMefEvokkmFyGqdthC92HqZ8mckqb71XejxAYmEf8Yzdh5ee89Ei0m8vLM4Bygxz_ic2LsnrkTGrJZqgwTZDSyGfSnd4sWDHAsGqIAhiZV7ZprBcUVnU24-JhTdvMyCzK8vIbeOh5Q4cqFbjbV7QdTsmuDEHj5H-4sA379nHNV0WdrEP8D6uoniDmowJZfSvpByizLCF7FDnWnL_nGo3GaGkvnQ'
+    auth_access_token_expires = '%222025-11-20T13%3A21%3A13.384Z%22'
+    refresh_token = 'c7NW9dqYwJuAhVVr3dA9CKDvmCGul25gFTxboBXN4SwFdms9UY2JGLtDTMWkGJnJcdkstvBeCl203CMjms'    
     
     # пример того, как можно перебирать слои в цикле и парсить по очереди.
     for layer in nspd_layers:
-        if layer['shortname'] == 'югра_доб_транс_газ':   # это просто чтобы парсить какой-то один слой, можно и убрать
-            access_token, refresh_token, auth_access_token_expires = download_nspd_layer(
-                nspd_layer=layer['id'],                     # идентификатор слоя, взятый из URL запроса на nspd.gov.ru/map
-                layer_alias=layer['shortname'],             # любое короткое имя слоя
-                tiles_gpkg='tiles.gpkg',
-                tiles_layer='test2',                        # В Geopackage должен быть этот слой с тайлами
-                width=128, height=128,                      # рекомендованные значения для размера тайла 128x128
-                i_from=0, i_to=128, j_from=0, j_to=128,     # в общем случае, должно совпадать с widht и height
-                pixel_step=9,                               # Для ХМАО рекомендовано 9. Для регионов поменьше можно 3.
-                access_token=access_token,                  # при первом запуске цикла берутся значения, заданные в разделе "ЗАМЕНИТЬ!!!". Потом будут браться автоматом из данных, возвращаемых функцией.
-                refresh_token=refresh_token,
-                auth_access_token_expires=auth_access_token_expires,
-                pause=0
-            )
-            print(f"access_token: {access_token}; refresh_token: {refresh_token}; auth_access_token_expires: {auth_access_token_expires}")
+        # if layer['shortname'] == 'югра_доб_транс_газ':   # это просто чтобы парсить какой-то один слой, можно и убрать
+        access_token, refresh_token, auth_access_token_expires = download_nspd_layer(
+            nspd_layer=layer['id'],                     # идентификатор слоя, взятый из URL запроса на nspd.gov.ru/map
+            layer_alias=layer['shortname'],             # любое короткое имя слоя
+            tiles_gpkg='nspd_zouit.gpkg',
+            tiles_layer='grid_zatop',                        # В Geopackage должен быть этот слой с тайлами
+            width=128, height=128,                      # рекомендованные значения для размера тайла 128x128
+            i_from=0, i_to=128, j_from=0, j_to=128,     # в общем случае, должно совпадать с widht и height
+            pixel_step=5,                               # Для ХМАО рекомендовано 9. Для регионов поменьше можно 3.
+            access_token=access_token,                  # при первом запуске цикла берутся значения, заданные в разделе "ЗАМЕНИТЬ!!!". Потом будут браться автоматом из данных, возвращаемых функцией.
+            refresh_token=refresh_token,
+            auth_access_token_expires=auth_access_token_expires,
+            pause=0
+        )
+        print(f"access_token: {access_token}; refresh_token: {refresh_token}; auth_access_token_expires: {auth_access_token_expires}")
 
 # Запуск скрипта:
 # 1. Установить uv https://docs.astral.sh/uv/getting-started/installation/
